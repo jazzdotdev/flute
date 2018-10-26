ee6d176f0a85a1f31ed49f2e417638b922e30ac1
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-10-26 01:30:00 -0400

    Actually use sorted_pairs (I forgot to do it)

--- package_loader.lua
+++ package_loader.lua
@@ -4,29 +4,6 @@
 -- foreach dir create specific path to events.txt, disabled_actions.txt, rules and actions
 -- 'trigger' the loaders
 
-function spairs(t, order)
-    -- collect the keys
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
-
-    -- if order function given, sort by it by passing the table and keys a, b,
-    -- otherwise just sort the keys 
-    if order then
-        table.sort(keys, function(a,b) return order(t, a, b) end)
-    else
-        table.sort(keys)
-    end
-
-    -- return the iterator function
-    local i = 0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
-end
-
 local log = require "log"
 local ansicolors = require 'ansicolors'
 
@@ -256,7 +233,7 @@ for k, v in pairs(fs.directory_list(packages_path)) do
 
 end
 
-for k,v in spairs(_G.rules_priorities, function(t,a,b) return t[b] < t[a] end) do
+for k,v in sorted_pairs(_G.rules_priorities, function(t,a,b) return t[b] < t[a] end) do
     table.insert(_G.rules, require(k))
 end
 --
