b847a3ab7e685ec5e9b0cd61aabada19ccd1afa2
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-11-11 19:24:40 +0100

    clean some comments

--- package_loader.lua
+++ package_loader.lua
@@ -23,172 +23,157 @@ local packages_path_length = #packages_path_modules
 -- require "lighttouch-libs.actions.create_key"
 package.path = package.path..";./packages/?.lua"
 
-package.preload["awdgb"] = function(name) print('test', name) end
-
-require "awdgb"
-
--- so i have to change package.preload[reqire_path] to change file while loading
 
 local default_package_searchers2 = package.searchers[2]
 package.searchers[2] = function(name) 
     if string.match( name, "rules") or string.match( name, "actions") then
         package.preload[name] = function(modulename)
-            log.warn("custom require")
             local created_file = io.open("tmp-lua/module.lua", "w+")
             local modulepath = string.gsub(modulename, "%.", "/")
-            --for path in string.gmatch(package.path, "([^;]+)") do
-                local path = "/"
-                local filename = string.gsub(path, "%?", modulepath)
-                -- for line in io.lines(modulepath .. ".lua") do
-                --     log.debug(line)
-                -- end
+            local path = "/"
+            local filename = string.gsub(path, "%?", modulepath)
+            local file = io.open(filename, "rb")
+            if file then
+                if string.match( name, "actions" ) then
 
-                local file = io.open(filename, "rb")
-                if file then
-                    if string.match( name, "actions" ) then
-
-                        local action_yaml = ""
-                        local line_num = 0
-                        for line in io.lines(modulepath .. ".lua") do
-                            line_num = line_num + 1        
-                            action_yaml = action_yaml .. line .. "\n" -- get only yaml lines
-                            if line_num == 3 then break end
-                        end
-                        local action_yaml_table = yaml.to_table(action_yaml) -- decode yaml to lua table
-                        created_file:write("local event = { \"" .. action_yaml_table.event[1] .. "\"") -- put values from yaml in lua form
-
-                        for _, yaml_event in ipairs(action_yaml_table.event) do
-                            if yaml_event ~= action_yaml_table.event[1] then 
-                                created_file:write(', "' .. yaml_event .. '"') -- put all events to 'local event = { }'
-                            end
-                        end
-                
-                        created_file:write(" }")
-                        created_file:write("\nlocal priority = " .. action_yaml_table.priority .. " \n\n")
-                        if action_yaml_table.input_parameters[1] then
-                            created_file:write("local input_parameters = { " .. "\"" .. action_yaml_table.input_parameters[1] .. "\"")
-                            for k, v in pairs(action_yaml_table.input_parameters) do
-                                if not table.contains(every_events_actions_parameters, v) then table.insert( every_events_actions_parameters, v ) end
-                                if k ~= 1 then
-                                    created_file:write(", \"" .. v .. "\"")
-                                end
-                            end
-                            created_file:write("}\n") 
-                        end
-                        created_file:write("local function action(arguments)\n") -- function wrapper
-                        
-                        for k, v in pairs(action_yaml_table.input_parameters) do
-                            created_file:write("\n\tlocal " .. v .. " = " .. "arguments[\"" .. v .. "\"]")
-                        end
-                        line_num = 0
-
-                        for line in io.lines(modulepath .. ".lua") do
-                            line_num = line_num + 1
-                            if line_num > 3 then
-                                created_file:write(line .. "\n\t")
-                            end
-                        end
-                        created_file:write("\nend\n\nreturn{\n\tevent = event,\n\taction = action,\n\tpriority = priority,\n\tinput_parameters = input_parameters\n}") -- ending return
-                        -- Compile and return the module
-                        created_file:close()
-
-                    elseif string.match( name, "rules") then
-
-                        local rule_yaml = ""
-                        local rule_yaml_table
-                        local line_num = 0
-                        for line in io.lines(modulepath .. ".lua") do
-                            line_num = line_num + 1        
-                            rule_yaml = rule_yaml .. line .. "\n" -- get only yaml lines
-                            if line_num == 3 then break end
-                        end
-
-                        rule_yaml_table = yaml.to_table(rule_yaml)
-
-                        local priority = rule_yaml_table.priority or 1
-                        if priority > 100 then priority = 100 end
-
-                        created_file:write("local priority = " .. priority)
-                        created_file:write("\nlocal events_table = { " .. "\"" .. rule_yaml_table.events_table[1] .. "\"")
-                        for k, v in pairs(rule_yaml_table.events_table) do
-                            if k ~= 1 then
-                                created_file:write(", " .. "\"" .. v .. "\"")
-                            end
-                        end
-                        created_file:write("}")
-                        created_file:write("\nlocal input_parameter = \"" .. rule_yaml_table.input_parameter .. "\"")
-                        created_file:write("\nlocal events_parameters = { }")
-                        created_file:write("\nlocal function rule(" .. rule_yaml_table.input_parameter)
-                        for k, v in pairs (every_events_actions_parameters) do
-                            if v ~= rule_yaml_table.input_parameter then 
-                                created_file:write(", " .. v)
-                            end
-                        end
-                        created_file:write(")")
-                        created_file:write("\n\tlog.debug('[Rule] " .. ansicolors('%{underline}' .. name) .. " with priority " .. priority .. " starting to evaluate')")
-                        created_file:write("\n\tlocal arguments_strings_dictionary = { }")
-                        created_file:write("\n\targuments_strings_dictionary[\"" .. rule_yaml_table.input_parameter .. "\"] = " .. rule_yaml_table.input_parameter)
-                        for k, v in pairs (every_events_actions_parameters) do -- matching rule arguments with action required parameters, so events_parameters["p1"] = p1
-                            if v ~= rule_yaml_table.input_parameter then 
-                                created_file:write("\n\targuments_strings_dictionary[\"" .. v .. "\"] = " .. v)
-                            end
-                        end
-            
-                        created_file:write("\n\tfor k, v in pairs(events_parameters) do")
-                        created_file:write("\n\t\tevents_parameters[k] = arguments_strings_dictionary[k]")
-                        created_file:write("\n\tend")
-                        created_file:write("\n\tif")
-                        line_num = 0
-                        for line in io.lines(modulepath .. ".lua") do
-                            line_num = line_num + 1
-                            if line_num > 3 then
-                                created_file:write("\n\t" .. line)
-                            end
-                        end
-
-                        created_file:write("\n\tthen")
-                        created_file:write("\n\t\tlog.trace(\"Rule " .. ansicolors('%{underline}' .. name) .. " evaluated as TRUE \")")
-                        created_file:write("\n\t\tfor k, v in pairs(events_table) do")
-                        created_file:write("\n\t\t\tevents[v]:trigger(events_parameters)")
-                        created_file:write("\n\t\tend")
-                        created_file:write("\n\telse")
-                        created_file:write("\n\t\tlog.trace(\"Rule " .. ansicolors('%{underline}' .. name) .. " evaluated as FALSE \")")
-                        created_file:write("\n\tend")
-            
-                        created_file:write("\n\tlog.debug('[Rule] " .. ansicolors('%{underline}' .. name) .. " evaluated succesfully')")
-                        created_file:write("\nend\n") -- bottom rule function wrapper
-                        
-                        created_file:write("\nlocal function get_events_parameters(events_actions)")
-                        created_file:write("\n\tfor k, v in pairs(events_table) do")
-                        created_file:write("\n\t\tfor k1, v1 in pairs(events_actions[v]) do")
-                        created_file:write("\n\t\t\tfor k2, v2 in pairs(v1.input_parameters) do")
-                        created_file:write("\n\t\t\t\tif not events_parameters[v2] then")
-                        created_file:write("\n\t\t\t\t\tevents_parameters[v2] = \" \"")
-                        created_file:write("\n\t\t\t\tend")
-                        created_file:write("\n\t\t\tend")
-                        created_file:write("\n\t\tend")
-                        created_file:write("\n\tend")
-                        created_file:write("\nend")
-            
-                        created_file:write("\nreturn{\n\trule = rule,\n\tpriority = priority,\n\tget_events_parameters = get_events_parameters\n}") 
-                        created_file:close()
-
-                        --return default_package_searchers2(name)
+                    local action_yaml = ""
+                    local line_num = 0
+                    for line in io.lines(modulepath .. ".lua") do
+                        line_num = line_num + 1        
+                        action_yaml = action_yaml .. line .. "\n" -- get only yaml lines
+                        if line_num == 3 then break end
                     end
-                    local to_compile = io.open("tmp-lua/module.lua", "rb")
-                    return assert(load(assert(to_compile:read("*a")), modulepath))
-                end
-            end
+                    local action_yaml_table = yaml.to_table(action_yaml) -- decode yaml to lua table
+                    created_file:write("local event = { \"" .. action_yaml_table.event[1] .. "\"") -- put values from yaml in lua form
 
-            return require(name)
+                    for _, yaml_event in ipairs(action_yaml_table.event) do
+                        if yaml_event ~= action_yaml_table.event[1] then 
+                            created_file:write(', "' .. yaml_event .. '"') -- put all events to 'local event = { }'
+                        end
+                    end
             
-            --return default_package_searchers2(name) -- tmp for now / it'll be deleted
+                    created_file:write(" }")
+                    created_file:write("\nlocal priority = " .. action_yaml_table.priority .. " \n\n")
+                    if action_yaml_table.input_parameters[1] then
+                        created_file:write("local input_parameters = { " .. "\"" .. action_yaml_table.input_parameters[1] .. "\"")
+                        for k, v in pairs(action_yaml_table.input_parameters) do
+                            if not table.contains(every_events_actions_parameters, v) then table.insert( every_events_actions_parameters, v ) end
+                            if k ~= 1 then
+                                created_file:write(", \"" .. v .. "\"")
+                            end
+                        end
+                        created_file:write("}\n") 
+                    end
+                    created_file:write("local function action(arguments)\n") -- function wrapper
+                    
+                    for k, v in pairs(action_yaml_table.input_parameters) do
+                        created_file:write("\n\tlocal " .. v .. " = " .. "arguments[\"" .. v .. "\"]")
+                    end
+                    line_num = 0
 
-        else
-            print(name) -- else default return so it won't change code of other modules (f.e. log or ansicolors)
-            return default_package_searchers2(name)
+                    for line in io.lines(modulepath .. ".lua") do
+                        line_num = line_num + 1
+                        if line_num > 3 then
+                            created_file:write(line .. "\n\t")
+                        end
+                    end
+                    created_file:write("\nend\n\nreturn{\n\tevent = event,\n\taction = action,\n\tpriority = priority,\n\tinput_parameters = input_parameters\n}") -- ending return
+                    created_file:close()
+
+                elseif string.match( name, "rules") then
+
+                    local rule_yaml = ""
+                    local rule_yaml_table
+                    local line_num = 0
+                    for line in io.lines(modulepath .. ".lua") do
+                        line_num = line_num + 1        
+                        rule_yaml = rule_yaml .. line .. "\n" -- get only yaml lines
+                        if line_num == 3 then break end
+                    end
+
+                    rule_yaml_table = yaml.to_table(rule_yaml)
+
+                    local priority = rule_yaml_table.priority or 1
+                    if priority > 100 then priority = 100 end
+
+                    created_file:write("local priority = " .. priority)
+                    created_file:write("\nlocal events_table = { " .. "\"" .. rule_yaml_table.events_table[1] .. "\"")
+                    for k, v in pairs(rule_yaml_table.events_table) do
+                        if k ~= 1 then
+                            created_file:write(", " .. "\"" .. v .. "\"")
+                        end
+                    end
+                    created_file:write("}")
+                    created_file:write("\nlocal input_parameter = \"" .. rule_yaml_table.input_parameter .. "\"")
+                    created_file:write("\nlocal events_parameters = { }")
+                    created_file:write("\nlocal function rule(" .. rule_yaml_table.input_parameter)
+                    for k, v in pairs (every_events_actions_parameters) do
+                        if v ~= rule_yaml_table.input_parameter then 
+                            created_file:write(", " .. v)
+                        end
+                    end
+                    created_file:write(")")
+                    created_file:write("\n\tlog.debug('[Rule] " .. ansicolors('%{underline}' .. name) .. " with priority " .. priority .. " starting to evaluate')")
+                    created_file:write("\n\tlocal arguments_strings_dictionary = { }")
+                    created_file:write("\n\targuments_strings_dictionary[\"" .. rule_yaml_table.input_parameter .. "\"] = " .. rule_yaml_table.input_parameter)
+                    for k, v in pairs (every_events_actions_parameters) do -- matching rule arguments with action required parameters, so events_parameters["p1"] = p1
+                        if v ~= rule_yaml_table.input_parameter then 
+                            created_file:write("\n\targuments_strings_dictionary[\"" .. v .. "\"] = " .. v)
+                        end
+                    end
+        
+                    created_file:write("\n\tfor k, v in pairs(events_parameters) do")
+                    created_file:write("\n\t\tevents_parameters[k] = arguments_strings_dictionary[k]")
+                    created_file:write("\n\tend")
+                    created_file:write("\n\tif")
+                    line_num = 0
+                    for line in io.lines(modulepath .. ".lua") do
+                        line_num = line_num + 1
+                        if line_num > 3 then
+                            created_file:write("\n\t" .. line)
+                        end
+                    end
+
+                    created_file:write("\n\tthen")
+                    created_file:write("\n\t\tlog.trace(\"Rule " .. ansicolors('%{underline}' .. name) .. " evaluated as TRUE \")")
+                    created_file:write("\n\t\tfor k, v in pairs(events_table) do")
+                    created_file:write("\n\t\t\tevents[v]:trigger(events_parameters)")
+                    created_file:write("\n\t\tend")
+                    created_file:write("\n\telse")
+                    created_file:write("\n\t\tlog.trace(\"Rule " .. ansicolors('%{underline}' .. name) .. " evaluated as FALSE \")")
+                    created_file:write("\n\tend")
+        
+                    created_file:write("\n\tlog.debug('[Rule] " .. ansicolors('%{underline}' .. name) .. " evaluated succesfully')")
+                    created_file:write("\nend\n") -- bottom rule function wrapper
+                    
+                    created_file:write("\nlocal function get_events_parameters(events_actions)")
+                    created_file:write("\n\tfor k, v in pairs(events_table) do")
+                    created_file:write("\n\t\tfor k1, v1 in pairs(events_actions[v]) do")
+                    created_file:write("\n\t\t\tfor k2, v2 in pairs(v1.input_parameters) do")
+                    created_file:write("\n\t\t\t\tif not events_parameters[v2] then")
+                    created_file:write("\n\t\t\t\t\tevents_parameters[v2] = \" \"")
+                    created_file:write("\n\t\t\t\tend")
+                    created_file:write("\n\t\t\tend")
+                    created_file:write("\n\t\tend")
+                    created_file:write("\n\tend")
+                    created_file:write("\nend")
+        
+                    created_file:write("\nreturn{\n\trule = rule,\n\tpriority = priority,\n\tget_events_parameters = get_events_parameters\n}") 
+                    created_file:close()
+                end
+                -- Compile and return the module
+                local to_compile = io.open("tmp-lua/module.lua", "rb")
+                return assert(load(assert(to_compile:read("*a")), modulepath))
+            end
         end
+
+        return require(name)
+    
+    else
+        print(name) -- else default return so it won't change code of other modules (f.e. log or ansicolors)
+        return default_package_searchers2(name)
     end
+end
 --
 
 events["lighttouch_loaded"] = luvent.newEvent()
@@ -272,9 +257,8 @@ for k, package_name in pairs (fs.directory_list(packages_path)) do
             if event then
                 table.insert( events_actions[v], action_require )
                 local action = event:addAction(
-                    function(action_arguments) -- ISSUE: we have to declare here as much arguments as the action needs(maybe do a table of arguments?)
+                    function(action_arguments)
                         log.debug("[running] action " .. ansicolors('%{underline}' .. file_name) .. " with priority " .. action_require.priority )
-                        -- TODO: figure out what to do if more than one responses are returned
                         possibleResponse = action_require.action(action_arguments)
                         if possibleResponse ~= nil then
                             if possibleResponse.body ~= nil then
@@ -318,8 +302,7 @@ for k, package_name in pairs(fs.directory_list(packages_path)) do
             local rule_require = require(rule_require_name)
             rule_require.get_events_parameters(events_actions) -- let the rule know which parameters it needs to its events actions
             log.debug("[loading] rule " .. ansicolors('%{underline}' .. rule_require_name))
-
-            --table.insert(_G.rules, rule_require)
+            
             _G.rules_priorities[rule_require_name] = rule_require.priority
         end
     end
