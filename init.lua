c7ca1a25dd5d6cc38b017d53d96386a1af05d26a
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-15 01:01:58 -0400

    Add uuids to client http messages, and return them in the wrapper function

--- init.lua
+++ init.lua
@@ -55,12 +55,14 @@ function _G.send_request (request)
     request = {uri = request}
   end
 
+  request.uuid = uuid.v4()
   events["outgoing_request_about_to_be_sent"]:trigger({ request = request })
 
   local response = client_request.send(request)
-
+  response.uuid = uuid.v4()
   events["incoming_response_received"]:trigger({ response = response })
   
+  return response
 end
 
 log.info("[loaded] LightTouch")
