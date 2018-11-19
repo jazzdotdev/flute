00cb586f50c1db9da2e75428609a996ae874b0c9
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-19 00:28:22 -0400

    Merge squash aleksanderwlodarczyk:content-splitting

---
+++ content/init.lua
@@ -0,0 +1,31 @@
+require "content.content_base"
+require "content.split_header"
+require "content.get_model_definition"
+require "content.get_validator"
+require "content.validate_document"
+require "content.read_document"
+require "content.documents"
+require "content.walk_documents"
+require "content.write_file"
+
+-- Methods for the model class
+local model_metatable = {}
+model_metatable.__index = model_metatable
+function model_metatable:validate (object)
+  for name, validator in pairs(self.fields) do
+    local result, err = validator(object[name])
+    if not result then
+      return result, name .. ": " .. err
+    end
+  end
+  return true
+end
+
+-- Add all directories in content to the stores table
+for entry in fs.entries("content/") do
+  if fs.metadata("content/" .. entry).type == "directory" then
+    content.stores[entry] = "content/" .. entry .. "/"
+  end
+end
+
+return content
