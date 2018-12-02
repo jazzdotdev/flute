76978dec9d7bafe3bb33db6d8917f925df9dae95
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-12-02 13:30:16 +0100

    moved non-functions from init.lua to base.lua

--- content/content_base.lua
+++ content/content_base.lua
@@ -5,6 +5,27 @@ content = {
   }
   local valua = require "third-party.valua"
   
+  -- Methods for the model class
+local model_metatable = {}
+model_metatable.__index = model_metatable
+function model_metatable:validate (object)
+  for name, validator in pairs(self.fields) do
+    local result, err = validator(object[name])
+    if not result then
+      return result, name .. ": " .. err
+    end
+  end
+  return true
+end
+
+-- Add all directories in content to the stores table
+for entry in fs.entries("content/") do
+  if fs.metadata("content/" .. entry).type == "directory" then
+    content.stores[entry] = "content/" .. entry .. "/"
+  end
+end
+
+
   fs.create_dir("content/home", true)
 
   
--- content/init.lua
+++ content/init.lua
@@ -7,24 +7,4 @@ require "content.documents"
 require "content.walk_documents"
 require "content.write_file"
 
--- Methods for the model class
-local model_metatable = {}
-model_metatable.__index = model_metatable
-function model_metatable:validate (object)
-  for name, validator in pairs(self.fields) do
-    local result, err = validator(object[name])
-    if not result then
-      return result, name .. ": " .. err
-    end
-  end
-  return true
-end
-
--- Add all directories in content to the stores table
-for entry in fs.entries("content/") do
-  if fs.metadata("content/" .. entry).type == "directory" then
-    content.stores[entry] = "content/" .. entry .. "/"
-  end
-end
-
 return content
