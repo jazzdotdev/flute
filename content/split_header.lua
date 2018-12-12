ecbcde6c28bf469ad50863118e7e5eed9baf7678
Hellfar <pierre.louchet@gmail.com>
2018-12-12 23:13:14 +0100

    split_header yaml to scl changing format.

--- content/split_header.lua
+++ content/split_header.lua
@@ -1,5 +1,5 @@
 function content.split_header (document_text)
-  local yaml_text, body = document_text:match("(.-)\n%.%.%.*\n?(.*)")
-  local header = yaml.to_table(yaml_text)
+  local scl_text, body = document_text:match("---\n(.*)\n...")
+  local header = scl.to_table(scl_text)
   return header, body
 end
