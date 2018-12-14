e65b6ed956a2ecfc07ac9b4db6cc2e74deb29519
Hellfar <pierre.louchet@gmail.com>
2018-12-14 02:08:55 +0100

    fix with empty header and setting body.

--- content/split_header.lua
+++ content/split_header.lua
@@ -1,5 +1,9 @@
 function content.split_header (document_text)
-  local yaml_text, body = document_text:match("(.-)\n%.%.%.*\n?(.*)")
-  local header = yaml.to_table(yaml_text)
+  log.trace("Running: " .. debug.getinfo(1, 'S').source)
+  local scl_text, body = document_text:match("---\n(.*)\n+...\n(.*)")
+  if scl_text == nil then
+    scl_text = ""
+  end
+  local header = scl.to_table(scl_text)
   return header, body
 end
