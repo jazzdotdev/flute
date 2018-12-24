fa837de3dab31cca9b4ddbc47870605361de6db3
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-12-24 13:38:26 -0400

    Load parent themes

--- loaders/theme/load_theme.lua
+++ loaders/theme/load_theme.lua
@@ -1,7 +1,23 @@
 function themes_loader.load_theme (name)
+  log.debug("[loading] theme " .. name)
+
   local path = themes_loader.dir .. name .. "/"
+
+  local info_content = fs.read_file(path .. "info.yaml")
+  if not info_content then
+    log.error("info.yaml not found in theme " .. name)
+    return
+  end
+  local info = yaml.to_table(info_content)
+
   local files = {}
   themes_loader.scan_subdir(files, "", path)
+
+  if info.parents then
+    for _, parent in ipairs(info.parents) do
+      themes_loader.load_theme(parent)
+    end
+  end
 
   -- Add the file list to the global themes list
   themes[name] = files
