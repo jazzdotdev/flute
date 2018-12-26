ee7e6df0f9e018368a8f71e1f97c3dc33ba3c7a3
root <root@localhost.localdomain>
2018-12-27 00:04:33 +0100

    refactor events loader

--- loaders/events/base.lua
+++ loaders/events/base.lua
@@ -1,3 +1,12 @@
 events_loader = { }
 
+require("loaders.events.mod")
+
+-- Actual Setup
+
+events_loader.create_base_events()
+
+-- Runs the function against all packages in packages_path
+each(fs.directory_list(_G.packages_path), events_loader.parse_events_lists)
+
 return events_loader
--- loaders/events/parse_events_lists.lua
+++ loaders/events/parse_events_lists.lua
@@ -1,8 +1,7 @@
-function events_loader.count_and_create_events (package_name)
+function events_loader.parse_events_lists (package_name)
     local events_strings = { } -- events names table
     local event_count = 0
     local package_path = _G.packages_path .. "/" .. package_name .. "/"
-    log.trace("[patching] actions for package " .. ansicolors('%{underline}' .. package_name))
 
     -- put each line into a strings array
     for line in fs.read_lines(package_path .. "events.txt") do
