a33a4e8718b58780b96046b827978258a5fe24d8
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2019-01-04 18:02:27 +0100

    merged with class loader

--- loaders/classes/process_files.lua
+++ loaders/classes/process_files.lua
@@ -9,11 +9,7 @@ function classes_loader.process_files(theme)
 
       local dir, basename, elem = matches:get(1), matches:get(2), matches:get(3)
       local filename = classes_loader.get_filename(dir, basename)
-<<<<<<< HEAD
       classes_loader.process_content(theme, filename, elem, classes)
-=======
-      classes_loader.process_content(theme, filename)
->>>>>>> 810b3c992a10cd1fabd90152238beace02937a30
 
     end
   end
