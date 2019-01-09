8c3f21f3021fe5143c1057cd6a9730166f14b6b3
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-10 04:26:30 +0530

    migrated to contentdb.lua

--- content/get_document_path.lua
+++
@@ -1,9 +0,0 @@
-function contentdb.get_document_path (doc_uuid, store_id)
-  if store_id then
-    return (contentdb.stores[store_id] or "contentdb/" .. store_id) .. doc_uuid
-  else
-    for store_id, dir in contentdb.stores_iter() do
-  
-    end
-  end
-end
--- content/read_document.lua
+++
@@ -1,8 +0,0 @@
-function contentdb.read_document (in_uuid)
-  log.trace("Running: " .. debug.getinfo(1, 'S').source)
-  return contentdb.walk_documents(nil, function (file_uuid, header, body, profile)
-    if file_uuid == in_uuid then
-      return header, body, profile
-    end
-  end)
-end
