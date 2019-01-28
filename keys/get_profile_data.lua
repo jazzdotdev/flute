c8c22ef9a2a7fa3ba6592f18da32230ca8991212
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2019-01-28 13:36:53 -0500

    get_place function, profile connection related functions, untested

---
+++ keys/get_profile_data.lua
@@ -0,0 +1,24 @@
+function keys.get_profile_data ()
+
+  local data = {}
+  local uuid, name = keys.get_profile()
+  log.debug("UUID: " .. tostring(uuid))
+
+  data.sign_public_key = content.walk_documents(uuid,
+    function (file_uuid, header, body)
+      if header.model == "key"
+      and header.kind == "sign_private"
+      then return body end
+    end
+  )
+
+  data.place = content.walk_documents(uuid,
+    function (file_uuid, header, body)
+      if header.model == "place" then
+        return header.host
+      end
+    end
+  )
+
+  return data
+end
